6cb575fd7f647c13cdbe7a72910e4aba0b4e1813
toastbin <a6046685@163.com>
2022-02-21 23:36:41 +0800

    feat: 封装track & trigger

--- src/reactivity/index.ts
+++ src/reactivity/index.ts
@@ -13,30 +13,42 @@ const reactivityProxy = <T extends object>(data: T): T => {
     new Proxy(data, {
         get(target, key) {
             if (!activeEffect) return
-
-            const depsMap = bucket.get(target)
-
-            if (!depsMap) {
-                depsMap.set(key, new Set())
-            }
-
-            const deps = depsMap.get(key)
-            deps.add(activeEffect)
-
+            // 追踪依赖变化
+            track(target, key)
             return target[key]
         },
         set(target, key, newVal) {
             target[key] = newVal
-            const depsMap = bucket.get(target)
-            
-            if (!depsMap) return
-            const effects = depsMap.get(key)
-            effects && effects.forEach(fn => fn())
+            trigger(target, key)
             return true
         }
     })
     
     return data
 }
+
+/** get 函数内调用，跟踪依赖变化 */
+const track = (target: object, key: string | number | symbol) => {
+    const depsMap = bucket.get(target)
+
+    if (!depsMap) {
+        depsMap.set(key, new Set())
+    }
+
+    const deps = depsMap.get(key)
+    deps.add(activeEffect)
+}
+
+/** set 函数内调用，触发副作用函数变化 */
+const trigger = (target: object, key: string | number | symbol) => {
+    const depsMap = bucket.get(target)
+
+    if (!depsMap) return
+
+    const effects = depsMap.get(key)
+    effects && effects.forEach(fn => fn())
+}
+
+Map
 
 export { effect, reactivityProxy }
